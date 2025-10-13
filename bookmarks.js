573a93edc768b6697f06c7cd77734300b92e46c9
Anna <awoellnitz@web.de>
2025-10-13 13:01:39 +0200

    newChapter3_13.10.25

--- bookmarks.js
+++ bookmarks.js
@@ -10,3 +10,33 @@ bookmark1.addEventListener("click", () => {
 bookmark2.addEventListener("click", () => {
     bookmark2.classList.toggle("paws1bookmark")
 })
+
+
+
+const rightButton1 = document.querySelector('[data-js="rightbutton1"]')
+rightButton1.addEventListener("click", () => {
+    rightButton1.classList.toggle("rightbuttonclick")
+})
+const falseButton1 = document.querySelector('[data-js="falsebutton1"]')
+falseButton1.addEventListener("click", () => {
+    falseButton1.classList.toggle("fakebuttonclick")
+})
+const falseButton11 = document.querySelector('[data-js="falsebutton11"]')
+falseButton11.addEventListener("click", () => {
+    falseButton11.classList.toggle("fakebuttonclick")
+})
+
+
+
+const rightButton2 = document.querySelector('[data-js="rightbutton2"]')
+rightButton2.addEventListener("click", () => {
+    rightButton2.classList.toggle("rightbuttonclick")
+})
+const falseButton2 = document.querySelector('[data-js="falsebutton2"]')
+falseButton2.addEventListener("click", () => {
+    falseButton2.classList.toggle("fakebuttonclick")
+})
+const falseButton22 = document.querySelector('[data-js="falsebutton22"]')
+falseButton22.addEventListener("click", () => {
+    falseButton22.classList.toggle("fakebuttonclick")
+})
--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 console.log("hallo")
 
 
+/* Textfeld für Question */
 const personalMessage = document.querySelector('[data-js="personal-message"]')
 const charactersCount = document.querySelector('[data-js="counter"]')
 
@@ -9,6 +10,7 @@ personalMessage.addEventListener("input", (event) => {
     charactersCount.innerText = event.target.maxLength - count;
 });
 
+/* Textfeld für Answer 1 */
 const personalMessage1 = document.querySelector('[data-js="personal-message1"]')
 const charactersCount1 = document.querySelector('[data-js="counter1"]')
 
@@ -17,6 +19,7 @@ personalMessage1.addEventListener("input", (event) => {
     charactersCount1.innerText = event.target.maxLength - count;
 });
 
+/* Textfeld für Answer 2 */
 const personalMessage2 = document.querySelector('[data-js="personal-message2"]')
 const charactersCount2 = document.querySelector('[data-js="counter2"]')
 
@@ -25,6 +28,7 @@ personalMessage2.addEventListener("input", (event) => {
     charactersCount2.innerText = event.target.maxLength - count;
 });
 
+/* Textfeld für Answer 3 */
 const personalMessage3 = document.querySelector('[data-js="personal-message3"]')
 const charactersCount3 = document.querySelector('[data-js="counter3"]')
 
@@ -34,22 +38,72 @@ personalMessage3.addEventListener("input", (event) => {
 });
 
 
+
+/* Formular */
 const form = document.querySelector('[data-js="message-form"]');
+
+/* Fieldset Personal Data */
 const firstInput = document.querySelector('[data-js="first-input"]');
 const lastInput = document.querySelector('[data-js="last-input"]');
-const messageContainer = document.querySelector('[data-js="message-container"]')
 
-const newElement = document.createElement("p");
-newElement.classList.add("card");
-newElement.textContent = firstInput.value;
-messageContainer.append(newElement);
+/* Platz für neue Fragenstellungen */
+const messageContainer = document.querySelector('[data-js="message-container"]');
 
+
+const newcardElement = document.createElement("div");
+newcardElement.classList.add("new-post");
+
+
+/* Auf den Button Submit klicken soll folgendes ausgeführt werden: */
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const newListElement = document.createElement("p");
-  newListElement.classList.add("card");
-  newListElement.textContent = firstInput.value;
-  messageContainer.append(newListElement);
-  event.target.reset();
+    event.preventDefault(); 
+    const newElement = document.createElement ("p");
+    newElement.textContent = firstInput.value;
+
+    const newElement2 = document.createElement ("p");
+    newElement2.textContent = lastInput.value;
+
+    const newElement3 = document.createElement ("p");
+    newElement3.textContent = personalMessage.value;
+
+    const newElement4 = document.createElement ("p");
+    newElement4.textContent = personalMessage1.value;
+
+    const newElement5 = document.createElement ("p");
+    newElement5.textContent = personalMessage2.value;
+
+    const newElement6 = document.createElement ("p");
+    newElement6.textContent = personalMessage3.value;
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    newcardElement.appendChild(newElement);
+    newcardElement.appendChild(newElement2)
+    newcardElement.appendChild(newElement3)
+    newcardElement.appendChild(newElement4)
+    newcardElement.appendChild(newElement5)
+    newcardElement.appendChild(newElement6)
+    form.append(newcardElement);
+    event.target.reset(); 
 });
 
+
+
+
+
+
+
+
+
+/* Fieldset mit firstInput/lastInput/personalMessage/personalMessage1/personalMessage2/personalMessage3 
+sollen ausgefüllt im Block messageContainer erstellt werden. Sobald ich den addbutton 'Submit' klicke */
+
+/* 
+1. Erstelle einen EventListener für das submit event auf dem Form Element.
+2. blockiere mithilfe eines preventDefault das senden an den Server.
+3. erstelle ein neues Element was unsichtbar im messageContainer erscheinen soll.
+4. suche die Eingaben des Users aus dem Formular raus und speichere sie in einer Variable ab. 
+5. fülle das neue hinzugefügte Element mit den korrekten HTML Elementen, nutze dafür die richtigen Variablen 
+an den richtigen Stellen.
+6. appende das Element in den Container
+7. beim betätigen des submit Button sollen die vorherigen Eingaben gelöscht werden*/
